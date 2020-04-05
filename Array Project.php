6c5c48fe05c7437b30ad48d9e2973d4983f1334c
Radio56 <60724135+Radio56@users.noreply.github.com>
2020-04-05 15:57:08 -0700

    Add files via upload

---
+++ Array Project.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$numbers = array("1", "5", "13", "35", "43", "58", "79");
+    echo count($numbers);
+        echo "Sum = " . array_sum($numbers) . "\n";
+            echo array_sum($numbers) / count($numbers)   
+?>
+
+</body>
+</html>
